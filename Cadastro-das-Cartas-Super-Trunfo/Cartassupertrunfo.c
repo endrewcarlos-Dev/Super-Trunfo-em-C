1e96d8d69da38c2947220f8f52284b96a6251a1c
Endrew_Carlos <endrewcarlos2006@gmail.com>
2025-04-08 22:48:09 -0300

    Super Trunfo em C final

--- Cadastro-das-Cartas-Super-Trunfo/Cartassupertrunfo.c
+++ Cadastro-das-Cartas-Super-Trunfo/Cartassupertrunfo.c
@@ -4,7 +4,6 @@
 
     int main (){
         
-        // Declaração das variáveis - CARTA 01
         int pontosturisticos;
         int regras, opcao;
         unsigned long int populacao;
@@ -17,7 +16,6 @@
         float pibpercapita;
         float Super_Poder;
 
-        // Menu Principal Com o uso do Switch
         printf("\n###### Bem-vindo ao Jogo Super Trunfo - Países ######\n");
         printf("Menu principal\n");
         printf("1. iniciar jogo\n");
@@ -26,7 +24,6 @@
         printf("Escolha uma opção: \n");
         scanf("%d", &opcao);
        
-        // Entrada das informações fornecidas pelo usuário DA CARTA 1
         switch (opcao)
         {
         case 1:
@@ -76,12 +73,10 @@
             break;
         }
 
-        // Cálculo da Densidade Populacional e do PIB per Capita CARTA 1
         densidadepopulacional = populacao / area;
         pibpercapita =  pib / populacao;
         Super_Poder = pib + pibpercapita + pontosturisticos + populacao + densidadepopulacional + area;
         
-        // Declaração das variáveis - CARTA 02
         int pontosturisticos2;
         unsigned long int populacao2;
         char estado2;
@@ -93,7 +88,6 @@
         float pibpercapita2;
         float Super_Poder2;
         
-        // Entrada das informações fornecidas pelo usuário DA CARTA 2
         printf("\n - CARTA 2 - \n");
         printf("Estado: B \n");
             
@@ -115,13 +109,11 @@
         printf("Digite o PIB: \n");
         scanf("%f", &pib2);
     
-        // Cálculo do Super Poder, Densidade Populacional e do PIB per Capita CARTA 2
         densidadepopulacional2 = populacao2 / area2;
         pibpercapita2 = pib2 / populacao2;
         Super_Poder2 = pib2 + pibpercapita2 + pontosturisticos2 + populacao2 + densidadepopulacional2 + area2;
         
         
-        // Saída das informações fornecidas pelo usuário DA CARTA 1 e CARTA 2
         printf("\n --CARTA 1-- \n - Estado: A%c\n - Código da Carta: %s\n - Nome da cidade: %s\n", estado, codigo, cidade);
         printf(" - População: %.2d\n - Número de Pontos Turísticos: %d\n - área: %.2f KM²\n - PIB: %.2f Bilhões de Reais\n", populacao, pontosturisticos, area, pib);
         printf(" - Densidade Populacional: %.2f hab/km²\n", densidadepopulacional);
@@ -132,7 +124,6 @@
         printf(" - Densidade Populacional: %.2f hab/km²\n", densidadepopulacional2);
         printf(" - PIB per Capita: %f REAIS\n", pibpercapita2);
             
-        // Comparação do resultado entre a Carta1 e a Carta2
         
         printf("\n=====QUEM GANHA? CARTA 1 OU CARTA 2?=====\n");
         if (pontosturisticos == pontosturisticos2)
